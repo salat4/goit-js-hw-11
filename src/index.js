022fddf289b208ac9ca1e7524aa6ba469e48cdc0
Dima Malik <mdima4266@gmail.com>
2022-01-06 20:33:06 +0200

    fix bag

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
-import simpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 
 import './sass/main.scss';
 const refs = {
@@ -10,7 +11,6 @@ const refs = {
     button: document.querySelector('button'),
     gallery: document.querySelector('.gallery'),
     loadMore: document.querySelector('.load-more'),
-    simpleLightbox: document.querySelector('.simple-lightbox'),
     body : document.querySelector('body')
 }
 
@@ -19,16 +19,21 @@ const KEY = '25099977-05a832f59cefe7e3a7990f935'
 const BASE_URL = 'https://pixabay.com/api/'
 const PARAMS_SEARCH = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=40'
 refs.form.addEventListener('submit', render)
+
 // refs.loadMore.addEventListener('click',loadMore )
 let page = 1;
 let qwe = 0;
 let totalPage = null;
 let asd = 0;
-// let count = 1;
-function render(e) {
-    console.log("page:", page)
-    console.log("asd:",asd)
+    
+let lightbox = new SimpleLightbox('.gallery a');
+
+
+async function render(e) {
+    
+    e.preventDefault(); 
     window.addEventListener("scroll", (() => {
+         e.preventDefault(); 
         let contentHeight = refs.gallery.offsetHeight;
         let yOffset = window.pageYOffset;
         let window_height = window.innerHeight;
@@ -36,19 +41,21 @@ function render(e) {
         if (y >= contentHeight) {
             page += 1
             takeBack(page)
-
+            lightbox.refresh();
         }
-        // return;
     })
     )
     // refs.loadMore.classList.add('visible')
+    // refs.gallery.addEventListener('click', ((e) => { 
+    // e.preventDefault();  
     refs.gallery.innerHTML = ""
-    e.preventDefault();
-            
-    
-        takeBack()
-            
+    takeBack()  
+    page = 1
+    // }))
+           
 }
+//let lightbox = new SimpleLightbox('.photo-card a')
+
  //   refs.loadMore.classList.remove('invis')
 
 
@@ -58,38 +65,28 @@ function render(e) {
     
 // }
 async function takeBack(page) { 
-  //  console.log(count)
+   
     
+
         await axios.get(`${BASE_URL}?key=${KEY}&q=${refs.input.value}&${PARAMS_SEARCH}&page=${page}`)
             .then((value) => {
                 totalPage = Math.ceil(value.data.totalHits / 40)
-                console.log("totalPage:", totalPage);
-                console.log("page:", page);
-                console.log("value.data.hits.length:",value.data.hits.length)
                 qwe = value.data.totalHits
                 asd = value.data.hits.length
-                
+               
                 if (page === undefined   && value.data.hits.length > 0) {
                     setTimeout(() => Notiflix.Notify.success(`Hoorey! We found ${qwe} images`), 500)
+                   // lightbox.refresh();
+                    
                 }
                 if (page >= totalPage+1) {
-                            
                             Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
                     return;
                         }
-                    
                     if (value.data.hits.length < 1) {
                         Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
                         return;
-                        }
-                        
-
-                        
-                       // console.log(qwe)
-                        
-                        
-               
-            
+                        }                                 
                     const Arr = value.data.hits.map((e) => {
                         return `<div class="photo-card">
                    <a href = ${e.largeImageURL}> <img src="${e.webformatURL}" alt="${e.tags}" loading="lazy" width = 640 height = 460 /> </a>
@@ -109,13 +106,10 @@ async function takeBack(page) {
                     </div>
                     </div>`
                     })
-        
-                    refs.gallery.insertAdjacentHTML("beforeend", Arr.join(""))
-               
-                
-         
-                let lightbox = new SimpleLightbox('.gallery a')
-            
-            })
+                refs.gallery.insertAdjacentHTML("beforeend", Arr.join("")) 
+              //  let lightbox = new SimpleLightbox('.photo-card a');
+              //  lightbox.close();
+                lightbox.refresh();
+                    })
     }
-     
+//
